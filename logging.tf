d251e09534e4757147742b97e52d9c56df9a2043
Jhon <jlagohuerta@amarello.com.mx>
2021-07-12 11:41:16 -0500

    Se regresa a 30 el retention

--- logging.tf
+++ logging.tf
@@ -1,6 +1,6 @@
 resource "google_logging_project_bucket_config" "basic" {
     project    = "amarello-interno-terraform"
     location  = "global"
-    retention_days = 400
+    retention_days = 30
     bucket_id = "_Default"
 }
